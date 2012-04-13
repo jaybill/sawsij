93406b1cf88e90981b932ae93727728ecd3a16a8
Jaybill McCarthy <heybill@jaybill.com>
2012-04-12 18:50:24 -0700

    Added go standard templating.

--- server.go
+++ server.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"net/http"
 	"os"
-    "fmt"
+    //"fmt"
     "strings"
+    "text/template"
+    "bytes"
 
 	"github.com/stathat/jconfig"
 	"launchpad.net/mgo"
-    "github.com/hoisie/mustache"
+
 )
 
 var context     *Context
@@ -36,16 +38,47 @@ func Route(pattern string,fn func(*http.Request, *Context) interface{}) {
 
 func makeHandler(fn func(*http.Request, *Context) interface{},templateId string ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fromHandler := fn(r, context)
+		handlerResults := fn(r, context)
 		
-		layoutId    := "layout"			
-		viewPath            := context.BasePath + "/views"						
-	    templateFilename    := viewPath + "/templates/" + templateId + ".html.ms"	    
-		layoutFilename      := viewPath + "/layouts/" + layoutId + ".html.ms"
-		rendered            := mustache.RenderFileInLayout(templateFilename, layoutFilename, fromHandler)
+		layoutId            := "layout"			
+		viewPath            := context.BasePath + "/views"							    
+		templateFilename    := templateId + ".html"
+		layoutFilename      := layoutId + ".html"
+	    templatePath        := viewPath + "/templates/" + templateFilename
+		layoutPath          := viewPath + "/layouts/" + layoutFilename
+				
+		tmpl, err := template.ParseFiles(templatePath,layoutPath)
+
+		alltemplates := tmpl.Templates()
+
+		for i :=0; i < len(alltemplates); i++ {
+    		log.Printf("template: %q",alltemplates[i].Name());
+		}
 		
-		fmt.Fprint(w,rendered)  
+        if err != nil { log.Print(err) }
+
+        // parse the content template first
+        content := new(bytes.Buffer)        
+        err = tmpl.ExecuteTemplate(content, templateFilename,handlerResults)
+        type ParsedContent struct{
+        	Content string		        
+        }
+        
+        var parsedContent ParsedContent
+        parsedContent.Content = content.String()
+        
+        if err != nil { 
+            log.Print(err) 
+        } else {
+            // take the results of parsing the content template and cram it into the layout template, then write that out
+            err = tmpl.ExecuteTemplate(w, layoutFilename,parsedContent)
+        }
 	}
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Serving static resource %q",r.URL.Path)	
+    http.ServeFile(w,r,context.BasePath + r.URL.Path)
 }
 
 func Configure() {
@@ -74,8 +107,8 @@ func Configure() {
 			
 	dbSession.SetMode(mgo.Monotonic, true)
 	context.DbSession = dbSession
-	
-
+    log.Print("Static dir is [" + context.BasePath + "/static" + "]")	
+    http.HandleFunc("/static/",staticHandler )
 }
 
 func Run() {
