6d929fa5c0b299338f975117aaa9d0421737f790
Jaybill McCarthy <heybill@jaybill.com>
2012-04-16 17:12:34 -0700

    Added model logic for talking to pg sql in db abstraction layer

--- model.go
+++ model.go
@@ -1,12 +1,56 @@
+/*
+Make sure the user's default schema is set
+ALTER USER jayblog SET search_path to 'jayblog'
+
+*/
+
 package sawsij
 
 import (
-/*
-	"time"
-    "log"
-*/
+	"database/sql"
+	_ "github.com/bmizerany/pq"
+	"log"
+	"reflect"
+	"strings"
 )
 
-type Thing struct {
-	Property string
+type Model struct {
+	Db *sql.DB
+}
+
+func (m *Model) Setup(db *sql.DB) {
+	m.Db = db
+}
+
+func (m *Model) Insert(data interface{}) {
+
+	s := reflect.ValueOf(data).Elem()
+	typeOfT := s.Type()
+
+	tableName := typeOfT.String()
+	parts := strings.Split(tableName, ".")
+	if len(parts) > 0 {
+		tableName = parts[len(parts)-1]
+	}
+	tableName = strings.ToLower(tableName)
+    
+    
+    
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		log.Printf("%d: %s %s = %v\n", i,
+			typeOfT.Field(i).Name, f.Type(), f.Interface())
+	}
+
+	query := "INSERT INTO " + tableName + "(title, body, createdon) VALUES ('Five Post', 'Even better than the first', current_timestamp)"
+
+	log.Printf("Query: %q", query)
+
+	result, err := m.Db.Query(query)
+	if err != nil {
+		log.Print(err)
+	} else {
+		log.Printf("Result was %v", result)
+	}
+
 }
--- server.go
+++ server.go
@@ -4,74 +4,81 @@ import (
 	"log"
 	"net/http"
 	"os"
-    
-    "strings"
-    "text/template"
-    
 
+	"strings"
+	"text/template"
+
+	"database/sql"
+	_ "github.com/bmizerany/pq"
 	"github.com/stathat/jconfig"
-	"launchpad.net/mgo"
-
 )
 
-var context             *Context
-var parsedTemplate      *template.Template
+var context *Context
+var parsedTemplate *template.Template
 
-func Route(pattern string,fn func(*http.Request, *Context) interface{}) {
-    
-    patternParts := strings.Split(pattern,"/")
-    var templateId string
+func Route(pattern string, fn func(*http.Request, *Context) interface{}) {
 
-    for i := 0; i < len(patternParts); i++ {
-		if i > 0{
-		    if patternParts[i] != "" {
-                templateId += patternParts[i] 
-		    } else {
-		        templateId += "index"
-		    }
-		    if(i < len(patternParts) - 1){
-		        templateId += "-"		   
-		    } 
+	patternParts := strings.Split(pattern, "/")
+	var templateId string
+
+	for i := 0; i < len(patternParts); i++ {
+		if i > 0 {
+			if patternParts[i] != "" {
+				templateId += patternParts[i]
+			} else {
+				templateId += "index"
+			}
+			if i < len(patternParts)-1 {
+				templateId += "-"
+			}
 		}
-	}  
-    http.HandleFunc(pattern, makeHandler(fn,templateId))
+	}
+	http.HandleFunc(pattern, makeHandler(fn, templateId))
 }
 
-func parseTemplates(){
-    viewPath            := context.BasePath + "/templates"
-    templateDir,err     := os.Open(viewPath)
-    if err != nil { log.Print(err) }
-    
-    allFiles,err   := templateDir.Readdirnames(0)
-    if err != nil { log.Print(err) }
-    templateExt   := "html"
-    var templateFiles []string
-    for i := 0; i < len(allFiles); i++ {
-       if si := strings.Index(allFiles[i], templateExt); si != -1{
-            if si == len(allFiles[i]) - len(templateExt){
-                templateFiles = append(templateFiles,viewPath + "/" + allFiles[i])
-            }
-        }
-    }
-    log.Printf("Templates: %v",templateFiles)
-    
-    pt, err := template.ParseFiles(templateFiles...)
-    parsedTemplate = pt
-    if err != nil { log.Print(err) }    
+func parseTemplates() {
+	viewPath := context.BasePath + "/templates"
+	templateDir, err := os.Open(viewPath)
+	if err != nil {
+		log.Print(err)
+	}
+
+	allFiles, err := templateDir.Readdirnames(0)
+	if err != nil {
+		log.Print(err)
+	}
+	templateExt := "html"
+	var templateFiles []string
+	for i := 0; i < len(allFiles); i++ {
+		if si := strings.Index(allFiles[i], templateExt); si != -1 {
+			if si == len(allFiles[i])-len(templateExt) {
+				templateFiles = append(templateFiles, viewPath+"/"+allFiles[i])
+			}
+		}
+	}
+	log.Printf("Templates: %v", templateFiles)
+
+	pt, err := template.ParseFiles(templateFiles...)
+	parsedTemplate = pt
+	if err != nil {
+		log.Print(err)
+	}
 }
 
-func makeHandler(fn func(*http.Request, *Context) interface{},templateId string ) http.HandlerFunc {
+func makeHandler(fn func(*http.Request, *Context) interface{}, templateId string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		handlerResults := fn(r, context)
-		templateFilename    := templateId + ".html"
-		err := parsedTemplate.ExecuteTemplate(w, templateFilename,handlerResults)
-        if err != nil { log.Print(err) }
-    }
+		templateFilename := templateId + ".html"
+		err := parsedTemplate.ExecuteTemplate(w, templateFilename, handlerResults)
+		if err != nil {
+			log.Print(err)
+		}
+	}
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
-    log.Printf("Serving static resource %q",r.URL.Path)	
-    http.ServeFile(w,r,context.BasePath + r.URL.Path)
+	log.Printf("Serving static resource %q", r.URL.Path)
+	http.ServeFile(w, r, context.BasePath+r.URL.Path)
 }
 
 func Configure() {
@@ -82,26 +89,25 @@ func Configure() {
 	if len(os.Args) == 1 {
 		log.Fatal("No basepath file specified.")
 	}
-    
-	context.BasePath    = string(os.Args[1]) 
-	configFilename      := context.BasePath + "/etc/config.json"
-	
+
+	context.BasePath = string(os.Args[1])
+	configFilename := context.BasePath + "/etc/config.json"
+
 	log.Print("Using config file [" + configFilename + "]")
 
 	c := jconfig.LoadConfig(configFilename)
 	context.Config = c
 
-	log.Print("Using db host [" + context.Config.GetString("mongo_host") + "]")
-	
-	dbSession, err := mgo.Dial(context.Config.GetString("mongo_host"))
+	db, err := sql.Open("postgres", context.Config.GetString("dbConnect"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	parseTemplates()		
-	dbSession.SetMode(mgo.Monotonic, true)
-	context.DbSession = dbSession
-    log.Print("Static dir is [" + context.BasePath + "/static" + "]")	
-    http.HandleFunc("/static/",staticHandler )
+	parseTemplates()
+
+	context.Db = db
+	log.Print("Static dir is [" + context.BasePath + "/static" + "]")
+	http.HandleFunc("/static/", staticHandler)
+
 }
 
 func Run() {
