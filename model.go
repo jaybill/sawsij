d816ba38e5955d116dd2f606cb6730109789b22f
Jaybill McCarthy <heybill@jaybill.com>
2012-04-17 17:47:19 -0700

    Got insert of arbitrary struct working

--- model.go
+++ model.go
@@ -8,6 +8,7 @@ package sawsij
 
 import (
 	"database/sql"
+	
 	_ "github.com/bmizerany/pq"
 	"log"
 	"reflect"
@@ -23,8 +24,41 @@ func (m *Model) Setup(db *sql.DB) {
 	m.Db = db
 }
 
-func (m *Model) Insert(data interface{}) {
+func (m *Model) Insert(data interface{}) (id int, err error) {
 
+    tableName   := getTableName(data)
+    keys, vals  := keysVals(data)
+    holders     := make([]string,len(keys))
+    
+    for i := 0; i < len(keys); i++{
+        holders[i] = fmt.Sprintf("$%v",i + 1)
+    }
+    
+	query := fmt.Sprintf("INSERT INTO %v(%v) VALUES (%v)",tableName,strings.Join(keys,","),strings.Join(holders,","))
+
+	log.Printf("Query: %q", query)	
+	_, err = m.Db.Exec(query,vals...)
+	if err != nil {
+		log.Print(err)
+	} else {
+	    idq := fmt.Sprintf("select currval('%v_id_seq')",tableName)	    
+	    row := m.Db.QueryRow(idq)
+	    if err != nil {
+		    log.Print(err)
+		} else {
+		    
+            err = row.Scan(&id)	
+            if err != nil{
+                log.Print(err)
+            } else {            
+    		    log.Printf("Id was %v", id)
+    		}
+		}
+	}
+	return    
+}
+
+func getTableName(data interface{}) string {
 	s := reflect.ValueOf(data).Elem()
 	typeOfT := s.Type()
 
@@ -33,29 +67,26 @@ func (m *Model) Insert(data interface{}) {
 	if len(parts) > 0 {
 		tableName = parts[len(parts)-1]
 	}
-	tableName = MakeDbName(tableName)
-    fieldNames  := []string{}
-    fieldValues := make([]interface{},0)
-    marks       := []string{}
+	return MakeDbName(tableName)
+}
+
+func keysVals(data interface{}) (keys []string, vals []interface{}){
+	s := reflect.ValueOf(data).Elem()
+	typeOfT := s.Type()
+
+
+    vals = make([]interface{},0)
+    
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
 		log.Printf("%d: %s %s = %v\n", i,typeOfT.Field(i).Name, f.Type(), f.Interface())
 		if typeOfT.Field(i).Name != "Id" {
-		    fieldNames = append(fieldNames,MakeDbName(typeOfT.Field(i).Name))
-		    fieldValues = append(fieldValues,f.Interface())
-		    marks = append(marks,"?")
+		    keys = append(keys,MakeDbName(typeOfT.Field(i).Name))
+		    vals = append(vals,f.Interface())
+		    
 		}
 	}
-    
-	query := fmt.Sprintf("INSERT INTO %v(%v) VALUES (%v)",tableName,strings.Join(fieldNames,","),strings.Join(marks,","))
-
-	log.Printf("Query: %q", query)
-    
-	result, err := m.Db.Exec(query,fieldValues)
-	if err != nil {
-		log.Print(err)
-	} else {
-		log.Printf("Result was %v", result)
-	}
-    
+    return
 }
+
+
