648db62de8d3138c147138bb84c19f454061547a
Jaybill McCarthy <heybill@jaybill.com>
2012-05-01 08:11:29 -0700

    Added new HandlerResponse struct to allow for redirects. Updated auth handler to test forwarding.

--- auth.go
+++ auth.go
@@ -1,16 +1,20 @@
 package sawsij
 
 import(    
-    "net/http"        
+    "net/http" 
+    "log"       
 )
 
-func LoginHandler(r *http.Request, c *Context, u map[string](string)) (view map[string](interface{}), err error) {
-	view = make(map[string]interface{})
+// LoginHandler is the built in method for handling auth. It can be used as is or called by a function you write that does
+// additional things, like SSO.
+func LoginHandler(r *http.Request, c *Context, u map[string](string)) (h HandlerResponse, err error) {
+	h.Init()
 
     if r.Method == "POST" {
+        log.Println("Checking username/password")
         if r.FormValue("username") == "jaybill" && r.FormValue("password") == "password"{
-        
-        }      
+            h.Redirect = "/admin"
+        }    
                  
     }
 
