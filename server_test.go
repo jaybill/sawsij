ffd0066643a0f48f929c6d243b89f25707f3dc90
Jaybill McCarthy <heybill@jaybill.com>
2012-05-15 07:31:56 -0700

    Loaded temp dir, added test

--- server_test.go
+++ server_test.go
@@ -4,3 +4,50 @@
 
 package sawsij
 
+import (
+	"os"
+	"strings"
+	"testing"
+	"flag"
+	"github.com/kylelemons/go-gypsy/yaml"
+)
+
+var env map[string]string
+var config *yaml.File
+var configFile = flag.String("c", "./config_test.yaml", "path to config file")
+var tmpdir string = ""
+var workdir string = ""
+
+func standup(t *testing.T) {
+	envStrings := os.Environ()
+
+	env = make(map[string]string)
+
+	for _, envString := range envStrings {
+		keyval := strings.Split(envString, "=")
+		env[keyval[0]] = keyval[1]
+	}
+
+	c, err := yaml.ReadFile(*configFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tmpdir, err := c.Get("server.tmpdir")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+    //workdir = tmpdir + "/sawsijtestapp"
+
+t.Log(tmpdir)
+
+	t.Log(os.TempDir())
+
+}
+
+func Test(t *testing.T) {
+	standup(t)
+
+}
+
