baa2948bb878b5771d19205232fb12637775ea06
Jaybill McCarthy <heybill@jaybill.com>
2012-05-17 07:28:07 -0700

    Added standup and teardown to test, writes test config file

--- server_test.go
+++ server_test.go
@@ -5,18 +5,50 @@
 package sawsij
 
 import (
+	"flag"
+	"github.com/kylelemons/go-gypsy/yaml"
 	"os"
 	"strings"
 	"testing"
-	"flag"
-	"github.com/kylelemons/go-gypsy/yaml"
 )
 
 var env map[string]string
 var config *yaml.File
 var configFile = flag.String("c", "./config_test.yaml", "path to config file")
-var tmpdir string = ""
-var workdir string = ""
+var workDir string = ""
+var staticDir string = "/static"
+var templateDir string = "/templates"
+var etcDir string = "/etc"
+var dummyConfigFile = `
+server:
+  port: 8077
+  cacheTemplates: false
+    
+database:
+  driver: @@DB_DRIVER@@
+  connect: @@DB_CONNECT@@
+    
+encryption:
+  salt: 213asjdhaskjh213
+  key: sakjdhuh23i123123
+`
+
+func writeStringToFile(input string,filepath string) (err error){
+
+ f, err := os.Create(filepath)
+    if err != nil {
+        return
+    }
+
+    defer f.Close()
+
+    _, err = f.Write([]byte(input))
+    if err != nil {
+        return 
+    }
+
+    return 
+}
 
 func standup(t *testing.T) {
 	envStrings := os.Environ()
@@ -28,26 +60,71 @@ func standup(t *testing.T) {
 		env[keyval[0]] = keyval[1]
 	}
 
+
+	workDir = os.TempDir() + "/sawsijtestapp"
+	//t.Log(workDir)
+
+	err := os.RemoveAll(workDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	staticDir = workDir + staticDir
+	templateDir = workDir + templateDir
+	etcDir = workDir + etcDir
+
+	err = os.MkdirAll(staticDir, os.FileMode(0777))
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	err = os.MkdirAll(templateDir, os.FileMode(0777))
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	err = os.MkdirAll(etcDir, os.FileMode(0777))
+	if err != nil {
+		t.Fatal(err)
+	}
+	
 	c, err := yaml.ReadFile(*configFile)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	tmpdir, err := c.Get("server.tmpdir")
+	dbDriver, err := c.Get("database.driver")
 	if err != nil {
 		t.Fatal(err)
 	}
+	
+	dbConnect, err := c.Get("database.connect")
+	if err != nil {
+		t.Fatal(err)
+	}
+    
+    dummyConfigFile = strings.Replace(dummyConfigFile, "@@DB_DRIVER@@", dbDriver, -1)
+    dummyConfigFile = strings.Replace(dummyConfigFile, "@@DB_CONNECT@@", dbConnect, -1)
+	
+	//t.Log(dummyConfigFile)
+	
+	err = writeStringToFile(dummyConfigFile,etcDir + "/config.yaml")
+	if err != nil{
+    	t.Fatal(err)
+	}
+			
 
-    //workdir = tmpdir + "/sawsijtestapp"
+}
 
-t.Log(tmpdir)
-
-	t.Log(os.TempDir())
-
+func teardown(t *testing.T) {
+	err := os.RemoveAll(workDir)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func Test(t *testing.T) {
 	standup(t)
-
+    teardown(t)
 }
 
