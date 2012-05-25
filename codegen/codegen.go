fb4cd43f8a646fa188f58b9a8258744173ac0237
Jaybill McCarthy <heybill@jaybill.com>
2012-05-24 17:05:57 -0700

    Added template parsing method. not working yet.

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -1,3 +1,18 @@
 package codegen
 
+import(
+    _"text/template"
+    _"io"
+)
 
+func ParseCodeTemplate(tpl string,data interface{}) (parsed string,err error){
+    /*
+    var rw io.ReadWriter
+    tmpl, err := template.New("dummy").Parse(tpl)
+    if err != nil { return  }
+    err = tmpl.Execute(rw, data)
+    if err != nil { return  }
+    */
+    return
+
+}
