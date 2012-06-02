554078be15792d8be9d76b70dbd65c994c3c942f
Jaybill McCarthy <heybill@jaybill.com>
2012-06-01 17:01:21 -0700

    Added copydir and copyfile functions. copydir not yet working.

---
+++ model.go
@@ -0,0 +1,258 @@
+// Copyright 2012 J. William McCarthy. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sawsij
+
+import (
+	"database/sql"
+
+	"fmt"
+	_ "github.com/bmizerany/pq"
+	"log"
+	"reflect"
+	"strings"
+)
+
+// The Model struct is intended to provide something analagous to a lightweight ORM, though not quite. 
+// The general pattern of usage is that you create a struct that represents a row in your table, with the 
+// fields mapping to column names. You then pass a pointer to that struct into Model's various methods 
+// to perform database operations. At the moment, only postgres is supported.
+//
+// Struct field names are converted to database column names using sawsij.MakeDbName() and database column
+// names are converted to struct field names using sawsij.MakeFieldName(). Generally, it works as follows:
+//
+// A struct field called "FirstName" will be mapped to a database column named "first_name".
+// A struct field called "Type" will be mapped to a database column named "type".
+//
+// Table names are mapped the same way, wherin a struct of type "PersonAddress" would look for a table called "person_address".
+//
+// At the moment only a single schema is supported and it must be in your database user's search path. You can generally do this 
+// in postgres with the following query:
+//
+// ALTER USER [db_username] SET search_path to '[app_schema_name]'
+// 
+// As current implemented, both your table and your struct must have an identity to do anything useful.
+type Model struct {
+	Db *sql.DB
+}
+
+// Update expects a pointer to a struct that represents a row in your database. The "Id" field of the struct will be used in the where clause.
+func (m *Model) Update(data interface{}) (err error) {
+
+	rowInfo := getRowInfo(data, false)
+	holders := make([]string, len(rowInfo.Keys))
+
+	for i := 0; i < len(rowInfo.Keys); i++ {
+		holders[i] = fmt.Sprintf("%v=$%v", rowInfo.Keys[i], i+1)
+	}
+
+	query := fmt.Sprintf("UPDATE %q SET %v WHERE id=%d", rowInfo.TableName, strings.Join(holders, ","), rowInfo.Id)
+	log.Printf("Query: %q", query)
+
+	_, err = m.Db.Exec(query, rowInfo.Vals...)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return
+}
+
+// Insert expects a pointer to a struct that represents a row in your database. The "Id" field of the referenced struct will be populated with the 
+// identity value if the row is successfully inserted.
+func (m *Model) Insert(data interface{}) (err error) {
+
+	rowInfo := getRowInfo(data, false)
+	holders := make([]string, len(rowInfo.Keys))
+
+	for i := 0; i < len(rowInfo.Keys); i++ {
+		holders[i] = fmt.Sprintf("$%v", i+1)
+	}
+
+	query := fmt.Sprintf("INSERT INTO %q(%v) VALUES (%v)", rowInfo.TableName, strings.Join(rowInfo.Keys, ","), strings.Join(holders, ","))
+
+	log.Printf("Query: %q", query)
+	log.Printf("Data: %+v", data)
+	_, err = m.Db.Exec(query, rowInfo.Vals...)
+	if err != nil {
+		log.Print(err)
+	} else {
+		if rowInfo.IdIndex != -1 {
+			idq := fmt.Sprintf("select currval('%v_id_seq')", rowInfo.TableName)
+			row := m.Db.QueryRow(idq)
+			if err != nil {
+				log.Print(err)
+			} else {
+				var id int64
+				err = row.Scan(&id)
+				if err != nil {
+					log.Print(err)
+				} else {
+					s := reflect.ValueOf(data).Elem()
+					s.Field(rowInfo.IdIndex).SetInt(id)
+					log.Printf("Id was %v", id)
+				}
+			}
+		}
+	}
+	return
+}
+
+// Delete takes a pointer to a struct and deletes the row where the id in the table is the Id of the struct.
+// Note that you don't need to have acquired this struct from a row, passing in a pointer to something like {Id: 4} will totally work.
+func (m *Model) Delete(data interface{}) (err error) {
+	rowInfo := getRowInfo(data, false)
+	if rowInfo.Id != -1 {
+		query := fmt.Sprintf("DELETE FROM %q WHERE id=%d", rowInfo.TableName, rowInfo.Id)
+		log.Printf("Query: %q", query)
+
+		_, err = m.Db.Exec(query)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	return
+}
+
+// Fetch returns a single row where the id in the table is the Id of the struct.
+func (m *Model) Fetch(data interface{}) (err error) {
+	rowInfo := getRowInfo(data, false)
+	cols := make([]interface{}, 0)
+	retRow := reflect.ValueOf(data).Elem()
+	dataType := retRow.Type()
+	if rowInfo.Id != -1 {
+		query := fmt.Sprintf("SELECT %v FROM %q WHERE id=%d", strings.Join(rowInfo.Keys, ","), rowInfo.TableName, rowInfo.Id)
+		log.Printf("Query: %q", query)
+		row := m.Db.QueryRow(query)
+
+		for i := 0; i < dataType.NumField(); i++ {
+			f := retRow.Field(i)
+			if dataType.Field(i).Name != "Id" {
+				cols = append(cols, f.Addr().Interface())
+			}
+		}
+		err = row.Scan(cols...)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	return
+}
+
+// The Query type is used to construct a SQL query. You should always use MakeDbName() to get column names, as this will 
+// ensure cross-RDBMS compatibility later on.
+type Query struct {
+	// A where clause, such as fmt.Sprintf("%v = 'Third Post'", MakeDbName("Title"))
+	Where string
+	// An order clause, such as fmt.Sprintf("%v DESC", MakeDbName("CreatedOn"))
+	Order string
+	// A number of records to limit the results to
+	Limit int
+	// The number of rows to offset the returned results by
+	Offset int
+}
+
+// FetchAll accepts a reference to a struct (generally "blank", though it doesn't matter), a Query and a set of query arguments and returns a set of rows that match
+// the query.
+func (m *Model) FetchAll(data interface{}, q Query, args ...interface{}) (ents []interface{}, err error) {
+	ents = make([]interface{}, 0)
+	rowInfo := getRowInfo(data, true)
+
+	retRow := reflect.ValueOf(data).Elem()
+	dataType := retRow.Type()
+	t := reflect.TypeOf(data).Elem()
+
+	query := fmt.Sprintf("SELECT %v FROM %q", strings.Join(rowInfo.Keys, ","), rowInfo.TableName)
+	if q.Where != "" {
+		query = fmt.Sprintf("%v WHERE %v", query, q.Where)
+	}
+
+	if q.Order != "" {
+		query = fmt.Sprintf("%v ORDER BY %v", query, q.Order)
+	}
+
+	if q.Limit != 0 {
+		query = fmt.Sprintf("%v LIMIT %v", query, q.Limit)
+	}
+
+	if q.Offset != 0 {
+		query = fmt.Sprintf("%v OFFSET %v", query, q.Offset)
+	}
+
+	log.Printf("Query: %q", query)
+
+	rows, err := m.Db.Query(query, args...)
+	if err == nil {
+		for rows.Next() {
+			ent := reflect.New(t)
+			cols := make([]interface{}, 0)
+			for i := 0; i < dataType.NumField(); i++ {
+				f := ent.Elem().Field(i)
+				cols = append(cols, f.Addr().Interface())
+			}
+			err = rows.Scan(cols...)
+
+			ents = append(ents, ent.Interface())
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	} else {
+		log.Print(err)
+	}
+	return
+}
+
+type forDb struct {
+	Id        interface{}
+	IdIndex   int
+	Keys      []string
+	Vals      []interface{}
+	TableName string
+}
+
+func getTableName(data interface{}) (tableName string) {
+	s := reflect.ValueOf(data).Elem()
+	typeOfT := s.Type()
+
+	tableName = typeOfT.String()
+	parts := strings.Split(tableName, ".")
+	if len(parts) > 0 {
+		tableName = parts[len(parts)-1]
+	}
+	tableName = MakeDbName(tableName)
+
+	return
+}
+
+func getRowInfo(data interface{}, includeId bool) (rowInfo forDb) {
+	s := reflect.ValueOf(data).Elem()
+	typeOfT := s.Type()
+	rowInfo.Vals = make([]interface{}, 0)
+
+	rowInfo.IdIndex = -1
+
+	rowInfo.TableName = typeOfT.String()
+	parts := strings.Split(rowInfo.TableName, ".")
+	if len(parts) > 0 {
+		rowInfo.TableName = parts[len(parts)-1]
+	}
+	rowInfo.TableName = getTableName(data)
+
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+
+		if typeOfT.Field(i).Name != "Id" || includeId {
+			dbName := MakeDbName(typeOfT.Field(i).Name)
+			rowInfo.Keys = append(rowInfo.Keys, dbName)
+			rowInfo.Vals = append(rowInfo.Vals, f.Interface())
+		}
+
+		if typeOfT.Field(i).Name == "Id" {
+			rowInfo.Id = f.Interface()
+			rowInfo.IdIndex = i
+		}
+
+	}
+	return
+}
---
+++ util.go
@@ -0,0 +1,349 @@
+// Copyright 2012 J. William McCarthy. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package sawsij
+
+import (
+	"archive/zip"
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+)
+
+// Return type constants, used in the switch for determining what format the response will be returned in.
+const (
+	RT_HTML = 0 // return HTML
+	RT_XML  = 1 // return XML
+	RT_JSON = 2 // return JSON
+)
+
+// MakeDbName converts a struct field name into a database field name. 
+// The string will be converted to lowercase, and any capital letters after the first one will be prepended with an underscore.
+// "FirstName" will become "first_name" and so on.
+func MakeDbName(fieldName string) string {
+	runes := []rune(fieldName)
+	copy := []rune{}
+	usrunes := []rune("_")
+	us := usrunes[0]
+	for i := 0; i < len(runes); i++ {
+		if i > 0 && unicode.IsUpper(runes[i]) {
+			copy = append(copy, us)
+		}
+		runes[i] = unicode.ToLower(runes[i])
+		copy = append(copy, runes[i])
+
+	}
+	return string(copy)
+}
+
+// GetIntId is a utility function for convertion a string into an int64. Useful for URL params.
+func GetIntId(strId string) (intId int64) {
+	intId, err := strconv.ParseInt(strId, 0, 0)
+
+	if err != nil {
+		intId = -1
+	}
+
+	return
+}
+
+// MakeDbName converts a database column name into a struct field name. 
+// The first letter will be made capital, and underscores will be removed and the following letter made capital.
+// "first_name" will become "FirstName", etc.
+func MakeFieldName(dbName string) string {
+
+	runes := []rune(dbName)
+	copy := []rune{}
+	usrunes := []rune("_")
+	us := usrunes[0]
+	for i := 0; i < len(runes); i++ {
+		if runes[i] != us {
+			if i == 0 {
+				runes[i] = unicode.ToUpper(runes[i])
+			}
+			copy = append(copy, runes[i])
+		} else {
+			runes[i+1] = unicode.ToUpper(runes[i+1])
+		}
+	}
+	return string(copy)
+}
+
+// GetUrlParams removes the string specified in "pattern" and returns key value pairs as a map of strings.
+func GetUrlParams(pattern string, urlPath string) (urlParams map[string]string) {
+	rp := strings.NewReplacer(pattern, "")
+	restOfUrl := rp.Replace(urlPath)
+
+	urlParams = make(map[string](string))
+	if len(restOfUrl) > 0 && strings.Contains(restOfUrl, "/") {
+		allUrlParts := strings.Split(restOfUrl, "/")
+
+		if len(allUrlParts)%2 == 0 {
+			for i := 0; i < len(allUrlParts); i += 2 {
+				urlParams[allUrlParts[i]] = allUrlParts[i+1]
+			}
+		}
+	}
+	return
+}
+
+// GetReturnType takes a pattern and determines the type of response being requested. Currently, "/json" is the only one implemented.
+func GetReturnType(url string) (rt int, restOfUrl string) {
+	jp := "/json"
+	if strings.Index(url, jp) == 0 {
+		jrp := strings.NewReplacer(jp, "")
+		restOfUrl = jrp.Replace(url)
+		rt = RT_JSON
+	}
+
+	xp := "/xml"
+	if strings.Index(url, xp) == 0 {
+		xrp := strings.NewReplacer(xp, "")
+		restOfUrl = xrp.Replace(url)
+		rt = RT_XML
+	}
+
+	if len(restOfUrl) == 0 {
+		restOfUrl = url
+		rt = RT_HTML
+	}
+
+	return
+}
+
+// GetTemplateName takes a URL pattern and returns a template Id as a string.
+func GetTemplateName(pattern string) (templateId string) {
+
+	patternParts := strings.Split(pattern, "/")
+	maxParts := len(patternParts)
+
+	if strings.LastIndex(pattern, "/") == len(pattern)-1 && len(pattern) > 1 {
+		maxParts = maxParts - 1
+	}
+
+	templateParts := make([]string, 0)
+	for i := 0; i < maxParts; i++ {
+		if i > 0 {
+			if patternParts[i] != "" {
+				templateParts = append(templateParts, patternParts[i])
+			} else {
+				templateParts = append(templateParts, "index")
+			}
+		}
+
+	}
+	templateId = strings.Join(templateParts, "-")
+
+	return
+}
+
+// Determines if int "needle" is in the array "haystack". Returns true if it is, false if it isn't.
+func InArray(needle int, haystack []int) (ret bool) {
+	ret = false
+
+	for i := 0; i < len(haystack); i++ {
+		if needle == haystack[i] {
+			ret = true
+			break
+		}
+	}
+
+	return
+}
+
+// Takes a string and a path and writes the string to the file specified by the path.
+func WriteStringToFile(input string, filepath string) (err error) {
+
+	f, err := os.Create(filepath)
+
+	if err != nil {
+		return
+	}
+
+	defer f.Close()
+
+	_, err = f.Write([]byte(input))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// Takes a string and a path and appends the string to the file specified by the path.
+func AppendStringToFile(input string, filepath string) (err error) {
+	
+    f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0744) 
+	if err != nil {
+		return
+	}
+
+	defer f.Close()
+
+	_, err = f.Write([]byte(input))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+
+// Takes a url and a file path. Downloads the url to the path.
+func CopyUrlToFile(url string, filepath string) (err error) {
+
+	f, err := os.Create(filepath)
+
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	res, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// Takes a path to a zip file and a directory to extract to, extracts the zip file, creating directories as needed.
+func UnzipFileToPath(zipfile string, path string) (err error) {
+	// Open a zip archive for reading.
+	r, err := zip.OpenReader(zipfile)
+	if err != nil {
+		return
+	}
+	defer r.Close()
+
+	// Iterate through the files in the archive
+	for _, f := range r.File {
+		var filepath string = ""
+		pathparts := strings.Split(f.Name, "/")
+
+		filename := pathparts[len(pathparts)-1]
+
+		for i := range pathparts {
+			if i < len(pathparts)-1 {
+				filepath += "/" + pathparts[i]
+			}
+		}
+
+		filepath = path + filepath
+
+		err = os.MkdirAll(filepath, os.FileMode(0777))
+		if err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		defer rc.Close()
+		if err != nil {
+			return err
+		}
+		outfile, err := os.Create(filepath + "/" + filename)
+		if err != nil {
+			return err
+		}
+
+		infile, err := ioutil.ReadAll(rc)
+		if err != nil {
+			return err
+		}
+
+		_, err = outfile.Write(infile)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return
+
+}
+
+// Returns a fixed length random identifier. NOT guaranteed to be globally unique. Useful for generating temporary directory names.
+func MakeRandomId() (ident string) {
+	rand.Seed(time.Now().UnixNano())
+	cr := strconv.FormatInt(int64(rand.Intn(999999999)+111111111), 10)
+	h := md5.New()
+	io.WriteString(h, cr)
+	ident = fmt.Sprintf("%x", h.Sum(nil))
+	return
+}
+
+func GetUserInput(prompt string, defaultAnswer string) (answer string, err error) {
+	var fmtPrompt string = ""
+
+	if defaultAnswer == "" {
+		fmtPrompt = "%v: %v"
+	} else {
+		fmtPrompt = "%v [%v]: "
+	}
+
+	fmt.Printf(fmtPrompt, prompt, defaultAnswer)
+	rd := bufio.NewReader(os.Stdin)
+	line, _, err := rd.ReadLine()
+
+	if err != nil {
+		return
+	} else {
+		answer = strings.TrimSpace(string(line))
+		if answer == "" {
+			answer = defaultAnswer
+		}
+	}
+
+	return
+
+}
+
+func CopyFile(dst, src string) (int64, error) { 
+        sf, err := os.Open(src) 
+        if err != nil { 
+                return 0, err 
+        } 
+        defer sf.Close() 
+        df, err := os.Create(dst) 
+        if err != nil { 
+                return 0, err 
+        } 
+        defer df.Close() 
+        return io.Copy(df, sf) 
+} 
+
+
+func CopyDir(s string,d string) (err error){
+	
+	entries, err := ioutil.ReadDir(s)
+	
+	for _, entry := range entries {
+		fullpath := s + "/" + entry.Name()
+		fmt.Printf("%v",fullpath)
+		if entry.IsDir(){
+			CopyDir(fullpath,d)
+		}		
+		fmt.Println()
+		
+	}
+	return
+}
+
