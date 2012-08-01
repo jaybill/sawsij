6a4fd512084a9ade60d8ab5b8d463cdf5ec19caa
Jaybill McCarthy <heybill@jaybill.com>
2012-08-01 16:07:36 -0700

    Merged weekly

--- framework/model/model.go
+++ framework/model/model.go
@@ -42,9 +42,9 @@ import (
 	"unicode"
 )
 
-// Table is the primary means of interaction with the database. It represents the access to a table, not the table itself. The package 
-// figures out what table to use based on the type being passed to the various methods of Table. Using anything but a 'flat' struct as a type
-// will have unpredictable results.
+// Table is the primary means of interaction with the database. It represents the access to a table, not the table itself. 
+// The package figures out what table to use based on the type being passed to the various methods of Table. 
+// Using anything but a 'flat' struct as a type will have unpredictable results.
 type Table struct {
 	// A handle to DbSetup type, which holds 
 	Db     *DbSetup
